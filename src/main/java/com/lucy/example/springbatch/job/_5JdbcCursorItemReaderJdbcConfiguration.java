025b4610743e9a5a7f2e5a9c7406c6888f38396b
luckylucy <luckylucy0717@gmail.com>
2021-08-31 16:53:08 +0900

    add Pay entity, use jdbcCursorItemReader

---
+++ src/main/java/com/lucy/example/springbatch/job/_5JdbcCursorItemReaderJdbcConfiguration.java
@@ -0,0 +1,65 @@
+package com.lucy.example.springbatch.job;
+
+import com.lucy.example.springbatch.entity.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class _5JdbcCursorItemReaderJdbcConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    private static final int chunkSize = 10;
+
+    @Bean
+    public Job jdbcCursorItemReaderJob() {
+        return jobBuilderFactory.get("jdbcCursorItemReaderJob")
+                .start(jdbcCursorItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcCursorItemReaderStep() {
+        return stepBuilderFactory.get("jdbcCursorItemReaderStep")
+                .<Pay, Pay>chunk(chunkSize)// 1: Reader에서 반환할 타입 2: Writer에 파라미터로 넘어올 타입 chunkSize: 트랜잭션 범위
+                .reader(jdbcCursorItemReader())
+                .writer(jdbcCursorItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Pay> jdbcCursorItemReader() {
+        return new JdbcCursorItemReaderBuilder<Pay>()
+                .fetchSize(chunkSize) // DB 에서 한번에 가져올 양 / Paging은 실제로 limit,offset을 사용하지만, Cursor는 fetchSize만큼 가져와 read()를 통해서 하나씩 가져옴
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+                .name("jdbcCursorItemReader")
+                .build();
+    }
+
+    private ItemWriter<Pay> jdbcCursorItemWriter() {
+        return list -> {
+            for (Pay pay: list) {
+                log.info("Current Pay = {}", pay);
+            }
+        };
+    }
+}
